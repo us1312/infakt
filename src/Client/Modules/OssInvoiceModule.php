105ce2bfa62fbaca9fd13a34f3d5f0344d210199
snajper167 <r.golawski@gmail.com>
2025-01-08 18:48:43 +0100

    feature(VatInvoice):add-read-to-vat-and-oss

--- src/Client/Modules/OssInvoiceModule.php
+++ src/Client/Modules/OssInvoiceModule.php
@@ -4,14 +4,14 @@ namespace SCA\InFakt\Client\Modules;
 
 class OssInvoiceModule extends BaseModule
 {
-    CONST ENDPOINT = '/oss_invoices.json';
+    CONST ENDPOINT = '/oss_invoices';
     CONST ASYNC = '/async';
     public function create(array $data): array {
-        return $this->request('POST', self::ENDPOINT, ['json' => $data]);
+        return $this->request('POST', self::ENDPOINT . '.json', ['json' => $data]);
     }
 
-    public function read(int $id): array {
-        return $this->request('GET', self::ENDPOINT . "/{$id}");
+    public function read(string $id): array {
+        return $this->request('GET', self::ENDPOINT . "/{$id}.json");
     }
 
     public function update(int $id, array $data): array {
